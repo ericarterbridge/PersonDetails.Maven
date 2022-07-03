96fd8a22a921f1a814eb8cb875054f3931f27c01
Eric <eric@zipcoders-mbp-6.myfiosgateway.com>
2022-07-03 18:21:22 -0400

    tests passed

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -13,7 +13,13 @@ public class PersonHandler {
     public String whileLoop() {
         String result = "";
         // create a `counter`
+        int counter = 0;
         // while `counter` is less than length of array
+        while(counter < personArray.length){
+            result += personArray[counter];
+            counter++;
+
+        }
             // begin loop
 
                 // use `counter` to identify the `current Person` in the array
@@ -31,7 +37,9 @@ public class PersonHandler {
         // identify initial value
         // identify terminal condition
         // identify increment
-
+        for(int i =0; i<personArray.length; i++){
+            result += personArray[i];
+        }
         // use the above clauses to declare for-loop signature
             // begin loop
                 // use `counter` to identify the `current Person` in the array
@@ -48,7 +56,9 @@ public class PersonHandler {
         String result = "";
         // identify array's type
         // identify array's variable-name
-
+        for (Person i : personArray){
+            result += i;
+        }
         // use the above discoveries to declare for-each-loop signature
             // begin loop
                 // get `string Representation` of `currentPerson`
